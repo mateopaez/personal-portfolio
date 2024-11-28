a88d03c44833fc2bab2c25ab72fb670718920cee
Mateo Paez <paezball@ualberta.ca>
2024-11-28 00:58:40 -0700

    finished navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import { useState } from 'react'
+import { navLinks } from '../constants/index.js'
+
+const NavItems = () => {
+  return (
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name}) => (
+        <li key={id} className="nav-li">
+          <a href={href} className="nav-li_a" onClick={() => {}}>
+            {name}
+          </a>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => setIsOpen( (prevIsOpen) => !prevIsOpen );
+
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
       <div className='max-w-7xl mx-auto'>
@@ -8,11 +27,25 @@ const Navbar = () => {
             <a href='/' className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
                 Mateo
             </a>
-            <button>
-                <img />
+            <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle menu">
+                <img src={
+                  isOpen 
+                  ? "assets/close.svg"
+                  : "assets/menu.svg"} alt="toggle" className="w-6 h-6"/>
             </button>
+            <nav className="sm:flex hidden">
+              <NavItems />
+            </nav>
         </div>
       </div>
+
+      {/* Mobile layout */}
+      <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+        <nav className="p-5">
+          <NavItems />
+        </nav>
+      </div>
+
     </header>
   )
 }
