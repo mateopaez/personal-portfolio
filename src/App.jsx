7cb1ec2fef773ae356eb8e0fa2a28e58995ada67
Mateo Paez <paezball@ualberta.ca>
2024-11-29 20:30:13 -0700

    made custom Button component, and started the About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import About from './components/About'
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
+      <About />
     </main>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,8 +30,8 @@ const Navbar = () => {
             <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle menu">
                 <img src={
                   isOpen 
-                  ? "assets/close.svg"
-                  : "assets/menu.svg"} alt="toggle" className="w-6 h-6"/>
+                  ? "src/assets/close.svg"
+                  : "src/assets/menu.svg"} alt="toggle" className="w-6 h-6"/>
             </button>
             <nav className="sm:flex hidden">
               <NavItems />
