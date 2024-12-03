b4ce79752e33385cf1e992cd5577c6596f1a8da2
Mateo Paez <paezball@ualberta.ca>
2024-12-02 17:51:44 -0700

    edited experience section and changed tab title

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -9,31 +9,30 @@ const WorkExperience = () => {
 
             {/* Main work Container */}
             <div className="work-container">
-            <div className="work-content">
-                <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-                {workExperiences.map((item, index) => (
-                    <div key={index} className="work-content_container group">
-                        
-                    {/* Logo and Content bar containers */}
-                    <div className="flex flex-col h-full justify-start items-center py-2">
-                        <div className="work-content_logo">
-                        <img className="w-full h-full rounded-2xl" src={item.icon} alt="" />
+                <div className="work-content">
+                    <div className="sm:py-10 py-5 sm:px-5 px-2.5">{workExperiences.map((item, index) => (
+                        <div key={index} className="work-content_container group">
+
+                            {/* Logo and Content bar containers */}
+                            <div className="flex flex-col h-full justify-start items-center py-2">
+                                <div className="work-content_logo">
+                                    <img className="w-full h-full rounded-2xl" src={item.icon} alt="" />
+                                </div>
+                                <div className="work-content_bar" />
+                            </div>
+                            
+                            {/* Work information (name, position, date, and title) */}
+                            <div className="sm:p-5 px-2.5 py-5">
+                                <p className="font-bold text-white-800">{item.name}</p>
+                                <p className="text-sm mb-5">
+                                    {item.pos} --- <span>{item.duration}</span>
+                                </p>
+                                <p className="group-hover:text-white transition-all ease-in-out duration-500">{item.title}</p>
+                            </div>
                         </div>
-                        <div className="work-content_bar" />
+                    ))} 
                     </div>
-                    
-                    {/* Work iformation (name, position, date, and title) */}
-                    <div className="sm:p-5 px-2.5 py-5">
-                        <p className="font-bold text-white-800">{item.name}</p>
-                        <p className="text-sm mb-5">
-                        {item.pos} --- <span>{item.duration}</span>
-                        </p>
-                        <p className="group-hover:text-white transition-all ease-in-out duration-500">{item.title}</p>
-                    </div>
-                    </div>
-                ))}
                 </div>
-            </div>
             </div>
         </div>
     </section>
